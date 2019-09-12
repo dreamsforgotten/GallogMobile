b8429db83cf2f9a83e4b9853293be306b04f8cc8
dreamsforgotten <middlepiller@gmail.com>
2019-09-12 08:16:12 -0500

    Lil API tweak goings on wit that JWT Token

--- GallogForms/GallogForms/Services/ApiFetcher.cs
+++ GallogForms/GallogForms/Services/ApiFetcher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using GallogForms.Models;
@@ -9,12 +10,31 @@ using Newtonsoft.Json;
 namespace GallogForms.Services
 {
     public static class ApiFetcher
-    {
+        {
+        public static string Token
+        {
+            get => Xamarin.Essentials.Preferences.Get(nameof(Token), "");
+            set => Xamarin.Essentials.Preferences.Set(nameof(Token), value);
+        }
+        public static string RefreshToken
+        {
+            get => Xamarin.Essentials.Preferences.Get(nameof(RefreshToken), "");
+            set => Xamarin.Essentials.Preferences.Set(nameof(RefreshToken), value);
+        }
+
         public static async Task<IEnumerable<Entry>> GetApiList()
         {
             using (var w = new HttpClient())
-            {
+            {                   
+              //  w.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("applicaiton/json"));
+              //  w.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("key", serverKey);
+
                 w.BaseAddress = new Uri("https://api.publicapis.org/");
+                if (!string.IsNullOrEmpty(Token))
+                {
+                    w.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+                }
+
                 return JsonConvert.DeserializeObject<ApiList>(await w.GetStringAsync("entries")).entries;
             }
         }
