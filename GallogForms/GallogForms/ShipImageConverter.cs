f22a2e3948a4d8102594214506d0cecfd5d957f9
Justin Mangum <parabola949@gmail.com>
2019-09-15 17:29:42 -0500

    push

---
+++ GallogForms/GallogForms/ShipImageConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GallogForms
+{
+    public class ShipImageConverter<T> : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"https://gallog.co/img/ships/{value}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString().Replace("https://gallog.co/img/ships/", "");
+        }
+    }
+}
--- Testing/Program.cs
+++ Testing/Program.cs
@@ -15,6 +15,9 @@ namespace Testing
             var c = new GallogClient("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcGkuZ2FsbG9nLmNvIiwiYXVkIjoiaHR0cDpcL1wvYXBpLmdhbGxvZy5jbyIsImlhdCI6MTM1Njk5OTUyNCwibmJmIjoxMzU3MDAwMDAwLCJkYXRhIjp7ImlkIjoiNjMiLCJ1c2VybmFtZSI6IkRyZWFtc2ZvcmdvdHRlbiIsImhhbmRsZSI6ImRyZWFtc2ZvcmdvdHRlbiIsImVtYWlsIjoibWlkZGxlcGlsbGVyQGdtYWlsLmNvbSJ9fQ.B7xcwG8Bn18UGT3xE-7Er3N-kY7vMViCE5u1EY-a4j8");
 
             var result = c.GetItemAsync<ShipResponse>("idris").Result;
+
+            Console.WriteLine(result.ship.image);
+            Console.Read();
          
         }
     }
