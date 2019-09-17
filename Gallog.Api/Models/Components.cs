6c130a38c550a2cd84d57768b9507f1bb69cac01
dreamsforgotten <middlepiller@gmail.com>
2019-09-17 08:48:14 -0500

    Added a few models to api

--- Gallog.Api/Models/Components.cs
+++ Gallog.Api/Models/Components.cs
@@ -7,13 +7,21 @@ namespace Gallog.Api.Models
 {
     [ApiPath("components")]
 
-
-
     public class Components : ApiQueryable
     {
-        public string message { get; set; }
-        public Ship ship { get; set; }
+        public Componenttype[] componentTypes { get; set; }
+    
+
+        public class Componenttype
+        {
+            public string id { get; set; }
+            public string categoryId { get; set; }
+            public string name { get; set; }
+            public string uri { get; set; }
+            public string icon { get; set; }
+            public string type { get; set; }
+        }
+
     }
 }
 
-    
