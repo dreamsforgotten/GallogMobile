89d5f66ce6ece5aca8e5d17b6c1c64c2f14e21d3
Dreamsforgotten <middlepiller@gmail.com>
2019-09-19 08:39:23 -0500

    Cleanup corrected syntax removed unwanted code

--- GallogForms/GallogForms/ViewModels/ComponentsViewModel.cs
+++ GallogForms/GallogForms/ViewModels/ComponentsViewModel.cs
@@ -8,18 +8,19 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 
 using Xamarin.Forms;
+using static Gallog.Api.Models.ComponentsList;
 
 namespace GallogForms.ViewModels
 {
     public class ComponentsViewModel : BaseViewModel
     {
         private GallogClient _gallogClient;
-        public ObservableCollection<ComponentsList.Componenttype> Items { get; set; }
+        public ObservableCollection<Componenttype> Items { get; set; }
         public Command RefreshItemsCommand { get; set; }
         public ComponentsViewModel()
         {
             Title = "Components";
-            Items = new ObservableCollection<ComponentsList.Componenttype>();
+            Items = new ObservableCollection<Componenttype>();
             _gallogClient = new GallogClient("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcGkuZ2FsbG9nLmNvIiwiYXVkIjoiaHR0cDpcL1wvYXBpLmdhbGxvZy5jbyIsImlhdCI6MTM1Njk5OTUyNCwibmJmIjoxMzU3MDAwMDAwLCJkYXRhIjp7ImlkIjo1NywidXNlcm5hbWUiOiJQYXJhIiwiaGFuZGxlIjoiUGFyYSIsImVtYWlsIjoicGFyYWJvbGE5NDlAZ21haWwuY29tIn19.bRpI9hVy-Spky5pbZhJCkyN-MT9RA6ap_yD9ezRxCxo");
             RefreshItemsCommand = new Command(async () => await ExecuteRefreshItemsCommand(), () => !IsBusy);
 
